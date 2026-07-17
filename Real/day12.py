957a2538989b7adee94f01b529cb1b9f4861b35c
agentforgeofficial <agentforgeofficial@gmail.com>
2026-07-18 00:00:06 +0500

    Added day12 info

--- Real/day12.py
+++ Real/day12.py
@@ -1,3 +1,6 @@
+#day 12: Implemented multi-user session memory using an in-memory dictionary, anchored the system prompt, 
+# and added sliding window context truncation to prevent token overflow.
+
 from fastapi import FastAPI
 from openai import OpenAI
 from dotenv import load_dotenv
@@ -23,7 +26,7 @@ async def chat_with_memory(request: ChatRequest):
     
     if sid not in session_db:
         session_db[sid] = [{"role" : "system", "content" : "You're a helpful assistant."}]
-    if len(session_db[sid]) > 11:
+    if len(session_db[sid]) > 11:      #here, len =length~
         session_db[sid] = [session_db[sid][0]] + session_db[sid][-10:]
     session_db[sid].append (
         {"role" : "user", "content" : request.topic}
