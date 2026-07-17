6903ed287c54ce968664f106e8d802d5f437ea6d
agentforgeofficial <agentforgeofficial@gmail.com>
2026-07-17 23:59:18 +0500

    Added day12 with multi user session memory through adding session id and more

---
+++ Real/day12.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI
+from openai import OpenAI
+from dotenv import load_dotenv
+from pydantic import BaseModel
+import os
+
+load_dotenv()
+app = FastAPI()
+client = OpenAI(
+    api_key=os.getenv("OPENROUTER_API_KEY"),
+    base_url="https://openrouter.ai/api/v1"
+)
+
+class ChatRequest(BaseModel):
+    session_id : str
+    topic : str
+
+session_db = {}
+
+@app.post("/chat")
+async def chat_with_memory(request: ChatRequest):
+    sid = request.session_id
+    
+    if sid not in session_db:
+        session_db[sid] = [{"role" : "system", "content" : "You're a helpful assistant."}]
+    if len(session_db[sid]) > 11:
+        session_db[sid] = [session_db[sid][0]] + session_db[sid][-10:]
+    session_db[sid].append (
+        {"role" : "user", "content" : request.topic}
+    )
+    
+    response = client.chat.completions.create(
+        model = "openrouter/free",
+        messages= session_db[sid],
+        # max_tokens=400
+    )
+    ai_response = response.choices[0].message.content
+    session_db[sid].append(
+        {"role" : "assistant", "content" : ai_response}
+    )
+    return{
+        "message" : ai_response
+    }
